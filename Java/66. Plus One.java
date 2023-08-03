946c547ffb4d0fc60eb69583bda49b635bf24ede
Gregory Kim <gkim@kimnetics.com>
2023-08-03 16:11:54 -0600

    Add problem 66.

---
+++ Java/66. Plus One.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean carry = true;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int number = digits[i];
+            if (carry) {
+                number++;
+                if (number > 9) {
+                    number = number - 10;
+                } else {
+                    carry = false;
+                }
+                digits[i] = number;
+            } else {
+                break;
+            }
+        }
+
+        if (carry) {
+            int[] output = new int[digits.length + 1];
+            output[0] = 1;
+            System.arraycopy(digits, 0, output, 1, digits.length);
+            return output;
+        } else {
+            return digits;
+        }
+    }
+}
