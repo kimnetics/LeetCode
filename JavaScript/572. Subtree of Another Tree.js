254112c5ea5633def293371f83f699ad531c3d95
Gregory Kim <gkim@kimnetics.com>
2024-03-10 17:17:12 -0600

    Add problem 572.

---
+++ JavaScript/572. Subtree of Another Tree.js
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/subtree-of-another-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+  var isSameTree = function (p, q) {
+    if ((p == null) && (q == null)) {
+      return true
+    }
+
+    if ((p == null) || (q == null)) {
+      return false
+    }
+
+    if (p.val !== q.val) {
+      return false
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  }
+
+  if (root == null) {
+    return false
+  }
+
+  if (isSameTree(root, subRoot)) {
+    return true
+  }
+
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
